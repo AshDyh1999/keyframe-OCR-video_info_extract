7332e8e1383879303c7ca1dcd4cabb4a6b4ec796
BB_Jack <35888877+AshDyh1999@users.noreply.github.com>
2019-11-30 18:45:32 +0800

    Update baidu_ocr_api.py

--- baidu_ocr_api.py
+++ baidu_ocr_api.py
@@ -10,11 +10,9 @@ desc: 调用百度OCRapi实现文本识别
 import base64
 from urllib import parse, request
 import json
- 
-# ak = '8de2c127fcfe427092c52f283adaf120'
-# sk = '74934f1992e54211871110650f9ea0ee'
-ak = 'v4ri9rThSC29iKyPufgCDUTa'
-sk = 'al9FONUC6aEynghjpODQXLGyCgibTeKD'
+
+ak = ''
+sk = ''
 
 def GetAccessToken(ak, sk):
     '''
